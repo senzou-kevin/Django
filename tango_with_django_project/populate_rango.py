8037cade6ba17b866a83987724e02789fd3d72ab
senzou-kevin <825484312@qq.com>
2021-07-29 09:03:38 +0200

    finished chap6 exercises

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -37,8 +37,8 @@ def populate():
          'views': 32}
     ]
 
-    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes':64},
-            'Django': {'pages': django_pages, 'views': 64, 'likes':32},
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
             'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
 
     for cat, cat_data in cats.items():
